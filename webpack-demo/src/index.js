d6fc1ed852108c4b8fe85658916c5f5a763aa382
sssssjy <1286847869@qq.com>
2020-12-04 09:58:17 +0800

    module id

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,18 +1,18 @@
 /*
  * @Author: your name
  * @Date: 2020-12-03 16:11:01
- * @LastEditTime: 2020-12-04 09:46:55
+ * @LastEditTime: 2020-12-04 09:55:36
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \webpack-learn\webpack-demo\src\index.js
  */
-async function getComponent() {
+import Print from './print'
+import _ from 'lodash'
+function getComponent() {
     const element = document.createElement('div');
-    const {default:_} = await import('lodash');
-    element.innerHTML = _.join(['hello','webpack'], ' ');
+    element.innerHTML = _.join(['hello','webpack'],' ');
+    element.onclick = Print.bind(null,'hello webpack click');
     return element;
 }
 
-getComponent().then((component) => {
-    document.body.appendChild(component);
-})
+document.body.appendChild(getComponent())
