4c988f0fbed7d66a7f44f29978125d6ec91de3e2
sssssjy <1286847869@qq.com>
2020-12-04 09:47:18 +0800

    webpack cacheGroups

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-03 16:11:01
- * @LastEditTime: 2020-12-03 17:02:56
+ * @LastEditTime: 2020-12-04 09:46:55
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \webpack-learn\webpack-demo\src\index.js
@@ -9,8 +9,6 @@
 async function getComponent() {
     const element = document.createElement('div');
     const {default:_} = await import('lodash');
-    const Prefetch = await import(/* webpackPrefetch: true */ './importComponent');
-    console.log(Prefetch);
     element.innerHTML = _.join(['hello','webpack'], ' ');
     return element;
 }
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-03 09:42:10
- * @LastEditTime: 2020-12-03 17:04:19
+ * @LastEditTime: 2020-12-04 09:41:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \document\webpack-demo\webpack.config.js
@@ -16,15 +16,30 @@ module.exports = {
         index:'./src/index.js',
     },
     output:{
-        filename:'[name].bundle.js',
+        filename:'[name].[contenthash].js',
         path:path.resolve(__dirname,'dist')
+    },
+    //提取入口公共代码
+    optimization:{
+        runtimeChunk:'single',
+        //将第三方库提取到单独的 vendor chunk文件中 
+        //这些文件不像本地源代码频繁修改 利用client长期缓存机制命中缓存消除请求 减少向server获取资源 并且保证client与server代码版本一致
+        splitChunks:{
+            cacheGroups:{
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
     },
     plugins:[
         new CleanWebpackPlugin({
             cleanStaleWebpackAssets:false
         }),
         new HtmlWebpackPlugin({
-            title:'import prefetch'
+            title:'Caching'
         })
     ]
 }
